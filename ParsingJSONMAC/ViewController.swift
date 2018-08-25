521482e08f00e2cbd774cc49a91c04c59b3d6d7c
Hajime Tousaka <hajimetousaka@sanzelians-macbook-air.local>
2018-08-25 21:16:08 +0800

    Initial Commit

---
+++ ParsingJSONMAC/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ParsingJSONMAC
+//
+//  Created by Hajime Tousaka on 25/8/2018.
+//  Copyright © 2018 Hajime Tousaka. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
